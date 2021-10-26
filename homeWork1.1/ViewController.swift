0e9309dd4e1769cef0bced678527bb9868507525
Artem Tkachev <tkachevforios@icloud.com>
2021-10-26 13:25:18 +0300

    Initial Commit

---
+++ homeWork1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork1.1
+//
+//  Created by Artem Tkachev on 26.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
